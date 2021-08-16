8693661366ae9c42a7e6618bf9befc961f67a853
Nicholas Khoo <xmencool1@gmail.com>
2021-08-16 17:33:47 +0800

    Initial Upload

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkay-hoo <nkay-hoo@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 16:30:44 by nkay-hoo          #+#    #+#             */
+/*   Updated: 2021/08/16 17:20:22 by nkay-hoo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE = 42;
+#endif
+
+#endif
